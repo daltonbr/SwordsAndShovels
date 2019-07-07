53756e86de6f44bf68478548e3068a6507a69e56
Dalton <daltonvarussa@gmail.com>
2019-07-07 18:30:08 +0200

    Layout basic structure for CharacterStats (using ScriptableObjects)

---
+++ Assets/2. Character Stats/Scripts/Monobehaviours/CharacterStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public CharacterStats_SO characterDefinition;
+    public CharacterInventory inventory;
+    public GameObject weaponSlot; // maybe a Transform would be better?
+}
---
+++ Assets/2. Character Stats/Scripts/ScriptableObjects/CharacterStats_SO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewStats", menuName = "Character/Stats", order = 1)]
+public class CharacterStats_SO : ScriptableObject
+{
+    
+}
