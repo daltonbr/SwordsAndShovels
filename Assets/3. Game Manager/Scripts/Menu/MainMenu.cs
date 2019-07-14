52a3f1056057d88b9ff4a24bd885bd72e43314c4
Dalton <daltonvarussa@gmail.com>
2019-07-14 19:52:55 +0200

    Transitioning Scenes
    Improved UIManager, MainMenu transitions with FadeIn and FadeOut animations

---
+++ Assets/3. Game Manager/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class MainMenu : MonoBehaviour
+{
+    // TODO: Function that can receive animation events
+
+    [SerializeField] private AnimationClip fadeOutAnimation;
+    [SerializeField] private AnimationClip fadeInAnimation;
+    private Animation _mainMenuAnimator;
+
+    private void Awake()
+    {
+        _mainMenuAnimator = GetComponent<Animation>();
+    }
+
+    public void OnFadeOutComplete()
+    {
+        Debug.Log("[MainMenu] FadeOut Complete");
+    }
+
+    public void OnFadeInComplete()
+    {
+        Debug.Log("[MainMenu] FadeIn Complete");
+
+        UIManager.Instance.SetDummyCameraActive(true);
+    }
+
+    public void FadeIn()
+    {
+        _mainMenuAnimator.Stop();
+        _mainMenuAnimator.clip = fadeInAnimation;
+        _mainMenuAnimator.Play();
+    }
+
+    public void FadeOut()
+    {
+        UIManager.Instance.SetDummyCameraActive(false);
+
+        _mainMenuAnimator.Stop();
+        _mainMenuAnimator.clip = fadeOutAnimation;
+        _mainMenuAnimator.Play();
+    }
+
+}
